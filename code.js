8aa50a9d25b4f808993d40f03c4c6be64a59df35
Sergio <sergio.ivan.melgarejo@gmail.com>
2021-10-16 16:17:41 -0300

    i fix the all problens... i think.

--- code.js
+++ code.js
@@ -1,59 +1,82 @@
 "use strict"
 
 const form = document.getElementById("calculator");
-const bill = document.querySelector(".input-1");
-const custom = document.querySelector(".input-custom");
-const people = document.querySelector(".input-2");
+const percentage = document.querySelector(".flex-percentage");
 const numberTip = document.querySelector(".numberTip");
 const numberTotal = document.querySelector(".numberTotal");
-const percentage = document.querySelector(".flex-percentage");
 const reset = document.querySelector(".button-reset");
+const custom = document.querySelector(".input-custom");
+//message
+const msgB = document.querySelector(".msg-b");
+const msgP = document.querySelector(".msg-p");
+const msgC = document.querySelector(".msg-c");
 
 //function
 const formData=()=>{
     let data = new FormData(form);
     let obj = {};
-    obj.b = parseInt(data.get("bill")) || 0;
-    obj.c = parseInt(data.get("custom")) || 0;
-    obj.p = parseInt(data.get("numerPeople")) || 0;
+    obj.b = data.get("bill");
+    obj.c = data.get("custom");
+    obj.p = data.get("numerPeople");
     return obj
 }
 
 const calculator =()=>{
     let data = formData();
-    if(data.b > 0){
-        numberTotal.textContent = "$" + parseInt(data.b);
-
-        if(data.c > 0){ 
-            numberTip.textContent = "$" + (data.b * data.c)/ 100;
-        }
-
-        if(data.p > 0){
-            numberTotal.textContent = "$" + data.b / data.p;
-            numberTip.textContent = "$" + (data.b * data.c)/ 100 / data.p;
-        }
-    }
-    else{   
-        numberTotal.textContent = "$0.00";
-        numberTip.textContent = "$0.00";
-    }
-}
+    console.log(data.b,typeof data.b, data.c,typeof data.c, data.p,typeof data.p)
+    numberTip.textContent = "$" + ((data.b / data.p) * data.c / 100) ;
+    numberTotal.textContent = "$" + ((data.b / data.p) + ((data.b / data.p) * data.c / 100));
+}   //.toFiced(2)...
 
 const verification = (event) =>{
     let value = parseInt(event.value);
-    if (value < 0)
+    if (value < 0){
         event.style.borderColor = "var(--color-2)";
-    else if (value > 0) event.style.borderColor = "var(--Strongcyan)";
-    else event.style.borderColor = "transparent";
+        if(event.classList.contains("input-1")){
+            msgB.innerHTML = "can't be negative";
+        }
+        else if(event.classList.contains("input-custom")){
+            msgC.innerHTML = "can't be negative";
+        }
+        else if(event.classList.contains("input-2")){
+            msgP.innerHTML = "can't be negative";
+        }
+    }
+    if(value == 0){
+        event.style.borderColor = "var(--color-2)";
+        if(event.classList.contains("input-1")){
+            msgB.innerHTML = "Can't be zero";      
+        }
+        else if(event.classList.contains("input-custom")){
+            msgC.innerHTML = "Can't be zero";   
+        }
+        else if(event.classList.contains("input-2")){
+            msgP.innerHTML = "Can't be zero";   
+        }
+    }
+    if (value > 0){
+        event.style.borderColor = "var(--Strongcyan)"
+        if(event.classList.contains("input-1")){
+            msgB.innerHTML = "";    
+        }
+        else if(event.classList.contains("input-custom")){
+            msgC.innerHTML = "";       
+        }
+        else if(event.classList.contains("input-2")){
+            msgP.innerHTML = "";    
+        }
+    }   
+    if(!value > 0 && !value < 0 && !value == 0){
+        event.style.borderColor = "transparent";
+    }
 }
 
 const resetAvtivated =()=>{
     let data = formData();
-    if(data.b && data.c && data.p){
+    if(data.b || data.c || data.p){
         reset.style.opacity = "1";
         reset.style.cursor = "pointer";
-        reset.style.backgroundColor = "var(--Strongcyan)";
-        
+        reset.style.backgroundColor = "var(--Strongcyan)";  
     }
     else {
         reset.style.opacity = ".3";
@@ -62,45 +85,54 @@ const resetAvtivated =()=>{
 }
 
 //event
-form.addEventListener("keyup", ()=>{
-    calculator();
-});
-
-bill.addEventListener("change",(e)=>{
+form.addEventListener("keyup", (e)=>{
     verification(e.target); 
-    resetAvtivated();
-
-});
-
-people.addEventListener("change",(e)=>{
-    verification(e.target); 
+    let data = formData();
+    if(data.b > 0 && data.c >= 0 && data.p > 0){
+        calculator();
+    }
     resetAvtivated();
 });
 
-custom.addEventListener("change",(e)=>{
-    verification(e.target); 
+custom.addEventListener("change", ()=>{
+    let data = formData();
+    if(data.b > 0 && data.c >= 0 && data.p > 0){
+        calculator();
+    }
     resetAvtivated();
 });
 
 percentage.addEventListener("click",(e)=>{
     if(e.target.classList.contains("percentage")){
         custom.value = parseInt(e.target.value);
-        let desactivar = e.target.parentNode.children;
-        for(let i = 0; i < desactivar.length -1; i++){
-            desactivar[i].classList.remove("active");
+        for(let i = 0; i < percentage.children.length -1; i++){
+            percentage.children[i].classList.remove("active");
         }
-        e.target.classList.add("active")
-        calculator()
+        e.target.classList.add("active");
+        verification(e.target)
+        let data = formData();
+        if(data.b > 0 && data.c >= 0 && data.p > 0){
+            calculator();
+        }
     }
 });
 
 reset.addEventListener("click",()=>{
     let data = formData();
-    if(data.b && data.p && data.c){
-        form.reset(); 
+    if(data.b || data.p || data.c){
         numberTotal.textContent = "$0.00";
         numberTip.textContent = "$0.00";
+        document.querySelectorAll(".inputs").forEach(e=>{
+            e.style.borderColor = "transparent";
+        });
+        for(let i = 0; i < percentage.children.length -1; i++){
+            percentage.children[i].classList.remove("active");
+        }
+        reset.style.backgroundColor = "var(--color-1)";
+        reset.style.opacity = ".3";
+            msgB.innerHTML = "";    
+            msgC.innerHTML = "";       
+            msgP.innerHTML = "";    
+        form.reset(); 
     }
 });
-
-//arreglar la cuenta, el porsentaje no debe cambiar lo que muesta input custom, evitar ejecutar cuenta si esta mal algo
